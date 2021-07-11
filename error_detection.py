64a7b0b38fadf67e6e4ea7f497dcd369debbc372
Salma Abdelfattah <47184333+salma77@users.noreply.github.com>
2021-07-11 20:26:20 +0200

    update error_detection.py

--- error_detection.py
+++ error_detection.py
@@ -3,25 +3,31 @@ import re
 def collect_tags(filename):
     #Parses the xml file and collects tags
     tags = []
+    line_num = 0
     with open(filename) as f:
         for line in f:
+            line_num = line_num + 1
             if line.find('<!') != -1: #A comment line
                 continue
             if line.find('<?') != -1: #A Declaration line
                 continue
             
             while line.find('<') != -1:
-                if line[line.find('<') + 1] != '/':
-                    open_tag = line[line.find('<'): line.find('>') + 1]
+                ind_start = line.find('<')
+                ind_end = line.find('>')
+                if line[ind_start + 1] != '/':
+                    open_tag = line[ind_start: ind_end + 1]
                     if '=' in open_tag:
                         open_tag = open_tag[: open_tag.find(' ')] + '>'
-                    tags.append(open_tag)
-                    line = re.sub(line[line.find('<'): line.find('>') + 1], ' ', line)
+                    tags.append([line_num, open_tag])
+                    line = line[:ind_start] + line[ind_end + 1:]
                          
                 if line.find('</') != -1:
-                    close_tag = line[line.find('</'): line.find('>') + 1]
-                    tags.append(close_tag)
-                    line = re.sub(line[line.find('</'): line.find('>') + 1], '', line)
+                    ind_start = line.find('</')
+                    ind_end = line.find('>')
+                    close_tag = line[ind_start: ind_end + 1]
+                    tags.append([line_num, close_tag])
+                    line = line[:ind_start] + line[ind_end + 1:]
                 
     while "" in tags:
         tags.remove("")   
@@ -31,17 +37,38 @@ def collect_tags(filename):
 def hasErrors(tags):
     #Checks if there are errors, returns the error tag
     queue = []
+    error_line = -1
+    errors = []
+    ind = 0
     for tag in tags:
-        if '/' not in tag: #An open tag
-            queue.append(tag)
-        elif '/' in tag:
-            if len(queue) > 0 and tag == '</' + queue[-1][1:]:
+        error_line = tag[0]
+        ind = ind + 1
+
+        if '/' not in tag[1]:    #Opening tag
+            queue.append(tag[1])
+
+        elif '/' in tag[1]:      #Closing tag
+            tag_names = []
+            for i in tags:
+                tag_names.append(i[1])
+            if len(queue) > 0 and tag[1] == '</' + queue[-1][1:]:
                 queue.pop()
+            # Tag opened with no closing
+            elif len(queue) > 0 and '</' + queue[-1][1:] not in tag_names[ind:]:
+                errors.append([queue[-1], error_line])
+                queue.pop()
+            # Tag closed with no opening
+            elif len(queue) == 0 or '<' + tag[1][2:] not in queue:
+                errors.append([tag, tag[0]])
+            # Tag mismatch
             else:
-                return True, queue[-1]
+                errors.append([tag, error_line])
+                
     if len(queue) == 0:            
         return False
-    return True, queue[-1]
+    # errors.append([queue[-1], error_line])
+    return errors
 
-# tags = collect_tags('file.xml')
-# print(hasErrors(tags))
+tags = collect_tags('file.xml')
+# print(tags)
+print(hasErrors(tags))
