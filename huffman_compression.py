2b82c77a0fecc5653271052d62d58960b8d31f61
Dina Abdul-Rasoul <73671062+dinaabdulrasoul@users.noreply.github.com>
2021-07-11 18:11:36 +0200

    refactoring

--- huffman_compression.py
+++ huffman_compression.py
@@ -6,9 +6,9 @@ Created on Fri Jul  9 15:28:50 2021
 """
 import sys
 # Changing python's default recursion depth so it won't cause us any errors while reading the files
-sys.setrecursionlimit(100000)
+sys.setrecursionlimit(100000000)
 
-class node(object):
+class node():
     """Huffman tree nodes implementation.
     A Huffman tree is basically a min heap/binary search tree.
     value: is the code assigned to the character
@@ -21,46 +21,86 @@ class node(object):
 
 class Compression():
     """Class that implements Huffman's Algorithm for compressing files."""
-    def createTree(self,nodes_list):
-        """Method that builds the huffman tree."""
+    def createTree(self,nodes):
+        """Method that builds the huffman(binary tree or a min heap)."""
         # First thing to do is sort the nodes in an ascending order
-        nodes_list.sort(key=lambda n: n.value)
-        length = len(nodes_list)   
-        if (length == 1):  # This means the tree only has 1 node
-            return nodes_list[0]    
+        nodes.sort(key=lambda n: n.value)
+        length = len(nodes)
+        
+        # Recursion stopping condition
+        if length == 1:
+            return nodes[0]
 
-        parent_node = node(nodes_list[0].value+nodes_list[1].value,nodes_list[0],nodes_list[1]) 
-        nodes_list[0].parent = nodes_list[1].parent 
-        nodes_list[1].parent = parent_node
+        # Merging leaf nodes into one node
+        left = nodes[0]
+        right = nodes[1]
+        value = nodes[0].value + nodes[1].value
+        new_node = node(value, left, right)
+
         # We delete the two nodes with the smallest frequency and use their sum to create a new parent node
-        nodes_list.pop(0)
-        nodes_list.pop(0)
-        nodes_list.insert(0,parent_node)   
-        return (self.createTree(nodes_list)) # Recursively call itself until the tree is formed
+        nodes[0].parent = nodes[1].parent
+        nodes[1].parent = new_node.parent
+        nodes.pop()
+        nodes.pop()
+        nodes.insert(0, new_node)
+
+        return (self.createTree(nodes)) # Recursively call itself until the tree is formed
 
     @staticmethod    
     def compress(self,file_path):
         """Interface method for the user to compress files."""
         self.compressFile(file_path)
-        
-    @staticmethod    
-    def extract(self,file_path):
-        """Interface function for the user to extract files."""
-        self.extractFile(file_path)
-
     
     def encodeNode(self,node):            
         """Method that encodes the leaf nodes."""
+        code = b''
         if (node.parent) == None:
-            return b''
+            return code 
         elif (node.parent.left) == node:
             return self.encodeNode(node.parent)+b'0'
         else:
             return self.encodeNode(node.parent)+b'1'
+
+    def createDicts(self, bytes_list):
+        """Method that creates necessary dictionaries that will be used to do the encoding"""
+        count = {}  # Dict to count the number of occurrences of each character
+        nodes_dict = {}  # dictionary that contains the nodes created out of the file characters
+        nodes = []      # List of nodes used to build the Huffman tree 
+        for i in bytes_list:
+            if i not in count.keys():
+                count[i] = 0
+            count[i] = count[i] + 1
+    
+        for j in count.keys():
+            nodes_dict[j] = node(count[j])  
+    
+        for x in nodes_dict.keys():
+            nodes.append(nodes_dict[x])
+        return bytes_list, count, nodes_dict, nodes
+
+    def maxFreq(self, count):
+        max_freq_count = 0
+        for j in count.keys():
+            if max_freq_count<count[j]:
+                max_freq_count = count[j]
+        w = 1 # width
+        one_byte = 255
+        two_bytes = 257 * one_byte
+        three_bytes = 65793 * one_byte
+
+        if max_freq_count > one_byte:
+            w = 2
+        elif max_freq_count > two_bytes:
+            w = 3
+        elif max_freq_count > three_bytes:
+            w = 4 
+        return w, max_freq_count
         
     def compressFile(self, input_file):
         """Method that encodes the input files using variable length prefix codes.
         The output is the encoded zip file."""
+
+        bytes_dict = {} # dictionary containing the encoded characters
 
         with open(input_file,'rb') as file:
             file.seek(0, 2)        
@@ -75,24 +115,8 @@ class Compression():
                     ind = ind + 1
                 else:
                     break
+        bytes_list, count, nodes_dict, nodes = self.createDicts(bytes_list)
 
-        count = {}  # Dict to count the number of occurrences of each character
-        nodes_dict = {}  # dictionary that contains the nodes created out of the file characters
-        nodes = []      # List of nodes used to build the Huffman tree 
-        bytes_dict = {} # dictionary containing the encoded characters
-
-        
-        for i in bytes_list:
-            if i not in count.keys():
-                count[i] = 0
-            count[i] = count[i] + 1
-    
-        for j in count.keys():
-            nodes_dict[j] = node(count[j])  
-    
-        for x in nodes_dict.keys():
-            nodes.append(nodes_dict[x])
-    
         root = self.createTree(nodes)  
 
         for x in nodes_dict.keys():
@@ -102,42 +126,42 @@ class Compression():
         file_name = input_file.split('/')[-1] # Extracting the file's name from the given path
 
         with open(path[0]+'.zip','wb') as object: # Creating the .zip file
-            object.write((file_name+'\n').encode(encoding='UTF-8'))
+            try:
+                object.write((file_name+'\n').encode(encoding='UTF-8'))
+            except OverflowError as err:
+                pass
             n = len(count.keys())
             object.write(int.to_bytes(n ,2 ,byteorder = 'big'))
 
             #First calculate the number of bytes occupied by the maximum frequency
-            max_freq_count =0
-            for j in count.keys():
-                if max_freq_count<count[j]:
-                    max_freq_count = count[j]
-            w = 1
-            if max_freq_count>255:
-                w = 2
-                if max_freq_count>65535:
-                    w = 3
-                    if max_freq_count>16777215:
-                        w = 4
-            
+            w, max_freq_count = self.maxFreq(count)
+
             object.write(int.to_bytes(w,1,byteorder='big'))
     
             for j in count.keys():
                 object.write(j)
-                object.write(int.to_bytes(count[j], w, byteorder = 'big'))
+                try:
+                    object.write(int.to_bytes(count[j], w, byteorder = 'big'))
+                except OverflowError as err:
+                    pass
     
-            code = b''   # prefix code used for encoding the characters
-            for j in bytes_list:
-                code += bytes_dict[j]
+            code = b''   # prefix code used for encoding the character
+
+            for byte in bytes_list:
+                code = code + bytes_dict[byte]
                 output = 0
-                while len(code)>=8:
-                    for j in range(8):
-                        output = output << 1
-                        if code[j] == 49:    
-                            #ASCII code of  1 is 49
-                            output = output | 1
-                    object.write(int.to_bytes(output,1,byteorder = 'big'))
-                    output = 0
-                    code = code[8:]
+                while(True):
+                        if len(code)>=8:
+                            for byte in range(8):
+                                output = output << 1
+                                if code[byte] == 49:    
+                                    #ASCII code of  1 is 49
+                                    output = output | 1
+                            object.write(int.to_bytes(output,1,byteorder = 'big'))
+                            output = 0
+                            code = code[8:]
+                        else:
+                            break
 
             #Processing any data that is less than one byte
             object.write(int.to_bytes(len(code), 1, byteorder = 'big')) 
@@ -147,18 +171,8 @@ class Compression():
                 if code[i] == 49:
                     output = output | 1
             object.write(int.to_bytes(output,1,byteorder='big'))
-            
 
-        
-
-        
 #Compression().compressFile("data-sample.xml")
-#Compression().compressFile("example_1.json")
-#Compression().compressFile("sample.json")            
-
-        
-
-        
-#Compression().compressFile("data-sample.xml")
+#Compression().compressFile("data.xml")
 #Compression().compressFile("example_1.json")
 #Compression().compressFile("sample.json")
